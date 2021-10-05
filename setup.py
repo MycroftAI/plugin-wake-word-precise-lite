b037abd753202dddf65ebf306ca918444b4b9575
Kris Gesling <kris.gesling@mycroft.ai>
2021-10-05 10:40:50 +0930

    Add package requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@ VERSION = '0.0.1'
 DESCRIPTION = 'Precise Runner for Mycroft'
 LONG_DESCRIPTION = 'Plugin module for Mycroft that supports Precise using tensorflow lite runtime'
 
+def required(requirements_file):
+    """Read requirements file and remove comments and empty lines."""
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base_dir, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
 setup(
     name="hotword_precise_lite",
     version=VERSION,
@@ -12,9 +20,6 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],
-    dependency_links=[
-        'https://google-coral.github.io/py-repo/ tflite_runtime'],
     entry_points={
         'mycroft.plugin.wake_word': 'hotword_precise_lite = hotword_precise_lite:TFLiteHotWord'},
     keywords=['mycroft', 'hot word', 'precise', 'lite', 'tensorflow'],
@@ -26,5 +31,6 @@ setup(
         "Operating System :: LINUX :: Linux",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    install_requires=required('requirements.txt')
 )
